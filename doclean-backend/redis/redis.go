5a393ca82d10c36b0be924f917b788049451e60a
bao_le <le.giabao@ecloudvalley.com>
2025-03-31 18:13:44 +0800

    feat: add redis connection

---
+++ doclean-backend/redis/redis.go
@@ -0,0 +1,20 @@
+package redis
+
+import "github.com/redis/go-redis/v9"
+
+func redis_connect() {
+	// ctx := context.Background()
+	// ctx := gin.Context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "memcached-15171.c285.us-west-2-2.ec2.redns.redis-cloud.com:15171",
+		Username: "default",
+		Password: "",
+		DB:       0,
+	})
+
+	return rdb
+
+	// rdb.Set(ctx, "foo", "bar", 0)
+
+}
